f6508477c80c3b5c59f9182340c241537a4f9442
Bayan Bokan <beckbokanov@gmail.com>
2021-01-29 16:32:59 +0600

    Второй урок сделан

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let money;
 let income;
-let addexpenses;
+let addExpenses;
 let deposit;
 let mission;
 let period;
@@ -8,3 +8,34 @@ let period;
 alert('Привет, Владислав :)');
 
 console.log('И тут тоже привет');
+
+money = 15000;
+income = 'Продажи книги';
+addExpenses = 'Продукты, развлечения, курсы';
+deposit = true;
+mission = 800000;
+period = 10;
+
+console.log('typeof money: ', typeof money);
+
+console.log('typeof deposit: ', typeof deposit);
+
+console.log('typeof income: ', typeof income);
+
+console.log('addExpenses.length: ', addExpenses.length);
+
+console.log(
+  'Период равен ' +
+    period +
+    ' месяцев. ' +
+    'Цель заработать ' +
+    mission +
+    ' рублей.'
+);
+//  здесь расширение Prettier отформатировало, пусть так останется или отключить / перенастроить его?
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+
+console.log('budgetDay: ', budgetDay);
