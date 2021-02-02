447dcea8bef558318445f52b4fd671862bf82b6d
Bayan Bokan <beckbokanov@gmail.com>
2021-02-02 17:12:46 +0600

    решение на функциях

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,67 @@
 'use strict';
 
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = +prompt('Ваш месячный доход?', 48000),
+  income = 'Продажи книги',
+  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
+    'Еда, квартира, кошачий корм'),
+  deposit = confirm('Есть ли у вас депозит в банке?'),
+  mission = 800000,
+  period = 10,
+  budgetDay;
 
-// alert('Привет, Владислав :)');
+const showTypeOf = function (data) {
+  console.log(data, typeof data);
+};
+// функция вывода типа данных в консоль
 
-// console.log('И тут тоже привет');
-
-money = 15000;
-income = 'Продажи книги';
-addExpenses = 'Продукты, развлечения, курсы';
-deposit = true;
-mission = 800000;
-period = 10;
-
-console.log('typeof money: ', typeof money);
-
-console.log('typeof deposit: ', typeof deposit);
-
-console.log('typeof income: ', typeof income);
-
-console.log('addExpenses.length: ', addExpenses.length);
-
-console.log(
-  'Период равен ' + period + ' месяцев. ' + 'Цель заработать ' + mission + ' рублей.');
+showTypeOf(money);
+showTypeOf(deposit);
+showTypeOf(income);
 
 console.log(addExpenses.toLowerCase().split(', '));
 
-let budgetDay = money / 30;
+let expenses1 = prompt('Введите обязательную статью расходов?', 'Коммуналка'),
+  amount1 = +prompt('Во сколько это обойдется?', 7540),
+  expenses2 = prompt('Введите обязательную статью расходов?', 'Транспорт'),
+  amount2 = +prompt('Во сколько это обойдется?', 6830);
 
-// Lesson 03
 
-money = +prompt('Ваш месячный доход?');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.');
-deposit = confirm('Есть ли у вас депозит в банке?');
+const getExpensesMonth = function (exp1, exp2) {
+  return exp1 + exp2;
+};
+let expensesMonth = getExpensesMonth(amount1, amount2);
+console.log(`Сумма всех обязательных расходов: ${expensesMonth}`);
+// функция подсчета суммы всех обязательных расходов
 
-let expenses1 = prompt('Введите обязательную статью расходов?'),
-  amount1 = +prompt('Во сколько это обойдется?'),
-  expenses2 = prompt('Введите обязательную статью расходов?'),
-  amount2 = +prompt('Во сколько это обойдется?');
+const getAccumulatedMonth = function (money, expenses) {
+  return money - expenses;
+};
+let accumulatedMonth = getAccumulatedMonth(money, expensesMonth);
+console.log(`Сумма накоплений за месяц: ${accumulatedMonth}`);
+// функция подсчета накоплений за месяц
 
-let budgetMonth = money - amount1 - amount2;
-console.log('Бюджет на месяц: ', budgetMonth);
-// рассчёт бюджета на месяц
+const getTargetMonth = function (accumulatedMonth) {
+  return Math.ceil(mission / accumulatedMonth);
+};
+let targetMonth = getTargetMonth(accumulatedMonth);
+console.log(`Период достижения цели: ${targetMonth} месяца(-ев)`);
+// функция подсчета периода достижения цели
 
-console.log(`Цель будет достигнута за ${Math.ceil(mission / budgetMonth)} месяцев (-a)`);
-// рассчёт месяцев для достижения цели
 
-budgetDay = Math.floor(budgetMonth / 30);
-console.log('Новый бюджет на день: ', budgetDay);
+budgetDay = Math.floor(accumulatedMonth / 30);
+console.log('Бюджет на день: ', budgetDay);
 // рассчет бюджета на день на основе бюджета на месяц
 
-if (budgetDay > 1200) {
-  console.log('У вас высокий уровень дохода');
-} else if (budgetDay >= 600 && budgetDay <= 1200) {
-  console.log('У вас средний уровень дохода');
-} else if (budgetDay >= 0 && budgetDay < 600) {
-  console.log('К сожалению, у вас уровень дохода ниже среднего');
-} else {
-  console.log('Что-то пошло не так');
-}
-// конструкция определения уровня дохода  
+const getStatusIncome = function () {
+  if (budgetDay > 1200) {
+    return ('У вас высокий уровень дохода');
+  } else if (budgetDay >= 600 && budgetDay <= 1200) {
+    return ('У вас средний уровень дохода');
+  } else if (budgetDay >= 0 && budgetDay < 600) {
+    return ('К сожалению, у вас уровень дохода ниже среднего');
+  } else {
+    return ('Что-то пошло не так');
+  }
+};
+console.log(getStatusIncome());
+// функция определения уровня дохода
